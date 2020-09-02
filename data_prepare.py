62c34cb24002e467f96cc4a5ea017cb4cce6e6f3
Akash Kumar <kmr2907akash@gmail.com>
2020-09-02 16:46:02 +0530

    Update data_prepare.py

--- data_prepare.py
+++ data_prepare.py
@@ -11,26 +11,12 @@ import pickle
 import matplotlib.pyplot as plt
 
 train_dir = listdir("train")
-# print(train_dir)
 images = []
 
 for sub_dir in train_dir:
-	# for img in listdir(join("train", sub_dir)):
-		# im = pickle.load(img)
-		# print(img)
-		# im = cv2.imread(join("train", sub_dir, img))
-		# # im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-		# print(im.shape)
-		# plt.imshow(im)
-		# plt.savefig("fig.png")
-		# break
-	# print(listdir(join("train", sub_dir)))
-	# print(glob.glob(join("train", sub_dir, '*.jpg\n')))
 	images += glob.glob(join("train", sub_dir, '*.jpg\n'))
-
-# print(images)
-# print(len(images))
-
+	
+# Train_data and train_label array
 train_data = []
 train_label = []
 count = 0
@@ -50,13 +36,6 @@ for j in images:
 
 train_data = np.array(train_data).astype("float32")
 train_label = np.array(train_label)
-# print(train_data.dtype)
-# print(train_data[0])
-# print(train_data[0]/255)
-# l = train_data[0]/255
-# print(l.dtype)
-# g = train_data[0].astype("float")
-# print(g/255)
 
 print(train_data.shape, train_label.shape)
 np.save("train_data.npy", train_data)
